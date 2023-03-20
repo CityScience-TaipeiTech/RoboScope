ff5c07dd09b79d2fe18b125d7bfbd37752ea0e1f
Justin Zhang <justincqz@ntut.edu.tw>
2023-03-20 17:10:58 +0800

    finish UdpServer2CANbus.py

--- CanbusController/README.md
+++ CanbusController/README.md
@@ -4,10 +4,26 @@
 2. Cando USB To CAN Module 
 
 ## UDP Testing with virtual CAN network (Linux only)
-To create a virtual can interface using SocketCAN run the following:
-```
-sudo modprobe vcan
-# Create a vcan network interface with a specific name(vcan0)
-sudo ip link add dev vcan0 type vcan
-sudo ip link set vcan0 up
-```
+1. Create a virtual can interface using SocketCAN run the following:
+    ``` bash
+    sudo modprobe vcan
+    # Create a vcan network interface with a specific name(vcan0)
+    sudo ip link add dev vcan0 type vcan
+    sudo ip link set vcan0 up
+    ```
+2. Running `UdpServer2CANbus.py` 
+    ``` bash
+    ./UdpServer2CANbus.py -c vcan0
+    ```
+3. Print all data that is being received by a CAN interface.
+    ``` bash
+    candump vcan0
+    ```
+4. Running `Json2UdpClient.py` to send a UDP data.
+    ``` bash
+    cd test
+    ./Json2UdpClient.py
+    ```
+    As result, the output of terminal at step 3 shows: 
+
+    ![image](../docs/vcan_test.png)
--- CanbusController/UdpServer2CANbus.py
+++ CanbusController/UdpServer2CANbus.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
+import datetime
 import argparse
 import socket
 import json
-# import can
+import can
 
 def arg_init():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--ip", type=str, help="UDP source ip address", default="127.0.0.1")
     parser.add_argument("-p", "--port", type=int, help="UDP receiving port on this machine", default=5005)
-    parser.add_argument("-c", "--can", type=str, help="CAN bus interface on this machine", default="")
+    parser.add_argument("-c", "--can", type=str, help="CAN bus interface on this machine", default="vcan0")
     return parser.parse_args()
 
 def udp_socket_init(ip, port):
@@ -17,16 +18,50 @@ def udp_socket_init(ip, port):
     print("UDP server starts receiving data at port ", port)
     return sock
 
-def udp_socket_receive(sock):
+def udp_socket_receive(sock, can_interface=None):
     while True:
         recieved = sock.recvfrom(1024)
-        json_obj = json.loads(recieved[0])
-        print(json_obj)
+        try:
+            json_obj = json.loads(recieved[0])
+        except ValueError as e:
+            print(e)
+            print("[", datetime.datetime.now(), "] Json loads failed, please make sure you json format is correct")
+            print("received raw data: ",  recieved[0])
+            return
+        # print("[", datetime.datetime.now(), "] ", json_obj)
+        
+        if(can_interface == None):
+
+            print("[", datetime.datetime.now(), "] Running in UDP test mode, and here this received UDP data: ")
+            print(json_obj)
+        else:
+            print("[", datetime.datetime.now(), "] ", "Received UDP data and sending out through SocketCAN...")
+            can_send_msg(can_interface, json_obj)
+
+def can_init(can_channel, bustype):
+    try:
+        bus = can.Bus(channel=can_channel, interface=bustype)
+        return bus
+    except OSError as e:
+        print(e)
+        print("Can not open", can_channel)
+        return None
+
+def can_send_msg(can_interface, msg):
+    offset = 10
+    for cell in msg["data"]:
+        # Row-major order
+        pos = cell["position"]
+        h = cell["height"]*10
+        id = 4*pos["y"]+pos["x"] + offset
+        msg = can.Message(arbitration_id=id, data=[0xff, h, 0, 0], is_extended_id=False)
+        can_interface.send(msg)
 
 def main():
     args = arg_init()
     sock = udp_socket_init(args.ip, args.port)
-    udp_socket_receive(sock)
+    can_interface = can_init(args.can, "socketcan")    
+    udp_socket_receive(sock, can_interface)
     return 0
 
 if __name__ == '__main__':

6c1c667cc3468ae62714e21bf075b0aba800cc07
Justin Zhang <justincqz@ntut.edu.tw>
2023-03-22 17:04:23 +0800

    add send and recieve UDP data at same time

--- CanbusController/test/Json2UdpClient.py
+++ CanbusController/test/Json2UdpClient.py
@@ -13,19 +13,20 @@ def arg_init():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--ip", type=str, help="UDP source ip address", default="127.0.0.1")
     parser.add_argument("-p", "--port", type=int, help="UDP receiving port on this machine", default=5005)
+    parser.add_argument("-f", "--file", type=str, help="Json file to send via UDP", default="./grasshopper_output.json")
     return parser.parse_args()
 
-def udp_socket_init(ip, port):
+def udp_socket_init():
     sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
     return sock
 
 
 def main():
     args = arg_init()
-    send_msg = get_json_fromfile("./grasshopper_output.json")
+    send_msg = get_json_fromfile(args.file)
     send_msg_str = json.dumps(send_msg)
     print("Sending message: ", json.dumps(send_msg, sort_keys=True, indent=4))
-    sock = udp_socket_init(args.ip, args.port)
+    sock = udp_socket_init()
     sock.sendto(send_msg_str.encode(),(args.ip, args.port))
     print("SENT to --> ", args.ip, " at port ", args.port)
     return 0
